a689657f247fe4e032871607eb75f59b0fa55a3c
Alexandre Russi - Mac <alexandrerussijunior@gmail.com>
2019-09-15 22:36:57 -0300

    Integração Firebase funcionando

--- AlphanumBraskem/src/app/places/discover/detail/detail.page.ts
+++ AlphanumBraskem/src/app/places/discover/detail/detail.page.ts
@@ -2,22 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 
+import { AngularFireDatabase } from '@angular/fire/database';
+
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.page.html',
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
+  private PATH = 'profiles/funcionario1/';
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  constructor(
+    private router: Router,
+    private navCtrl: NavController,
+    private db: AngularFireDatabase
+    ) { }
 
   ngOnInit() {
   }
 
   onBookPlace() {
     // this.router.navigateByUrl('/places/tabs/discover');
-    this.navCtrl.navigateBack('/places/tabs/discover');
+    // this.navCtrl.navigateBack('/places/tabs/discover');
     // this.navCtrl.pop(); nao tem garantia
+    const itemRef = this.db.object(this.PATH);
+    itemRef.set({chamar: '1'});
   }
 
 }
