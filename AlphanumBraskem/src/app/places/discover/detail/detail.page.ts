2fe654c3a563f334fd23831df5e99ecca630325b
Alexandre Russi - Mac <alexandrerussijunior@gmail.com>
2019-09-18 10:14:57 -0300

    Maps Screen

--- AlphanumBraskem/src/app/places/discover/detail/detail.page.ts
+++ AlphanumBraskem/src/app/places/discover/detail/detail.page.ts
@@ -4,6 +4,8 @@ import { NavController } from '@ionic/angular';
 
 import { AngularFireDatabase } from '@angular/fire/database';
 
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.page.html',
@@ -11,6 +13,7 @@ import { AngularFireDatabase } from '@angular/fire/database';
 })
 export class DetailPage implements OnInit {
   private PATH = 'profiles/funcionario1/';
+  map: Map;
 
   constructor(
     private router: Router,
@@ -21,12 +24,41 @@ export class DetailPage implements OnInit {
   ngOnInit() {
   }
 
-  onBookPlace() {
+  chamarAlphanumcar() {
     // this.router.navigateByUrl('/places/tabs/discover');
     // this.navCtrl.navigateBack('/places/tabs/discover');
     // this.navCtrl.pop(); nao tem garantia
     const itemRef = this.db.object(this.PATH);
     itemRef.set({chamar: '1'});
+  }
+
+  ionViewDidEnter() {
+    this.leafletMap();
+  }
+
+  leafletMap() {
+    this.map = new Map('mapId').setView([-12.654430, -38.317934], 15);
+
+    tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+      attribution: 'Alphanum'
+    }).addTo(this.map);
+
+    // const markPoint = marker([-12.654430, -38.317934]);
+    // markPoint.bindPopup('PV-14 Braskem').openPopup();
+    // this.map.addLayer(markPoint);
+    const planta1 = marker([-12.654430, -38.317934]);
+    planta1.addTo(this.map)
+      .bindPopup('PV-14 Braskem')
+      .openPopup();
+
+    const planta2 = marker([-12.6541916, -38.3203977]);
+    planta2.addTo(this.map)
+      .bindPopup('PE3 Braskem')
+      .openPopup();
+  }
+
+  ionViewWillLeave() {
+    this.map.remove();
   }
 
 }
