c4dfe976b832c13e92eb903c291911198b17e8ad
Alexandre Russi - Mac <alexandrerussijunior@gmail.com>
2019-09-25 15:36:05 -0300

    New inputs - Firebase

--- AlphanumBraskem/src/app/places/offers/offers.page.ts
+++ AlphanumBraskem/src/app/places/offers/offers.page.ts
@@ -6,6 +6,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 import { AngularFireDatabase } from '@angular/fire/database';
+import { Key } from 'protractor';
 
 @Component({
   selector: 'app-offers',
@@ -23,7 +24,10 @@ export class OffersPage implements OnInit {
   private PATH = 'profiles/funcionario1/';
   offers: Place[];
   isDisabled = true;
-  firstName = '';
+  firstName: string;
+  dataNasc: string;
+  rg: string;
+  senha: string;
 
   constructor(
       private placesService: PlacesService,
@@ -34,17 +38,36 @@ export class OffersPage implements OnInit {
     const x: OffersPage = this;
     // this.offers = this.placesService.places;
     // const itemRef = this.db.object(this.PATH);
+    const keys = [];
+    const counts = [];
     this.db.database.ref(this.PATH).orderByChild('nome').once('value', (snapshot) => {
       // tslint:disable-next-line: only-arrow-functions
       snapshot.forEach(function(child) {
-        console.log(child.val());
-        x.setFirstName(child.val());
+        // console.log(child.val());
+        keys.push(child.val());
         });
+      console.log(keys);
+      x.setDataNasc(keys[0]);
+      x.setFirstName(keys[1]);
+      x.setRg(keys[2]);
+      x.setSenha(keys[3]);
       });
+  }
+
+  setDataNasc(dataNasc: string) {
+    this.dataNasc = dataNasc;
   }
 
   setFirstName(nome: string) {
     this.firstName = nome;
+  }
+
+  setRg(rg: string) {
+    this.rg = rg;
+  }
+
+  setSenha(senha: string) {
+    this.senha = senha;
   }
 
   enableInputs() {
@@ -55,25 +78,24 @@ export class OffersPage implements OnInit {
     this.isDisabled = true;
   }
 
-  editarPerfil() {
-    // Recupera nome no input
-    const nomeInput = document.getElementById('nomeInput').getElementsByTagName('input')[0];
-
-    // Referencia para caminho firebase
-    const itemRef = this.db.object(this.PATH);
-    itemRef.set({nome: nomeInput});
-  }
-
   onSubmit(form: NgForm) {
     if (!form.valid) {
       return;
     }
     const nomeInput = form.value.nome;
-    console.log(form);
+    const dataNascInput = form.value.dataNasc;
+    const rgInput = form.value.rg;
+    const senhaInput = form.value.senha;
+    // console.log(form);
 
     // Referencia para caminho firebase
     const itemRef = this.db.object(this.PATH);
-    itemRef.set({nome: nomeInput});
+    itemRef.set({
+      nome: nomeInput,
+      dataNasc: dataNascInput,
+      rg: rgInput,
+      senha: senhaInput,
+    });
 
     this.disableInputs();
     console.log(this.isDisabled);
